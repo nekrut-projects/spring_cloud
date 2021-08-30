7cc1298949485fbf08c634026814e14f30ca66b3
nekrut <nekrut12@gmail.com>
2021-08-30 18:56:22 +0300

    Initial commit

---
+++ src/test/java/ru/gb/spring_cloud/SpringCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.spring_cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
