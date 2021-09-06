d3978a07bb5e4d159464894a2b31286164d06bd6
nekrut <nekrut12@gmail.com>
2021-09-06 16:28:58 +0300

    lesson_6

--- products_storage/src/main/java/ru/gb/storage/exceptions/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ru.gb.storage.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
